7e95452d29f6b2456f854aeaab48283fd1e2f4ef
MathisD49 <mathisdubuc@gmail.com>
2021-10-12 10:38:47 +0200

    ajout d'une methode pour attraper un pokemon

--- object/game.py
+++ object/game.py
@@ -4,7 +4,7 @@ from pokemon import Pokemon
 from pokeball import Pokeball
 
 class Game:
-    def __init__(self, pokemon_liste, nb_spawn):
+    def __init__(self, pokemon_liste, inventaire_objets, nb_spawn):
         self.pokemon_liste = pokemon_liste
         self.total_percent_pokemon = 0
         self.calcul_total_percent()
@@ -12,8 +12,9 @@ class Game:
         self.percent_data = []
         self.nb_spawn = nb_spawn
         self.inventaire_pokemon = []
-        self.inventaire_objets = []
+        self.inventaire_objets = inventaire_objets
         self.solde_pokedollars = 0
+        self.verif = 0
 
     def calcul_total_percent(self):
         for i in self.pokemon_liste:
@@ -50,6 +51,20 @@ class Game:
             else:
                 print(self.pokemon_liste[percent_pokemon].name, "apparait autant de fois que prévu (", str(self.percent_data[percent_pokemon]), " au lieu de ", str(real_percent), ")")
 
+    def pokeball_choice(self):
+        for i in self.inventaire_objets:
+            print(str(self.inventaire_objets.index(i)+1), " - ", i.name)
+        print("99 - quitter")
+        choix = int(input("Quel pokeball voulez-vous ? : "))
+
+        if choix == 99:
+            self.verif += 1
+        elif choix > len(self.inventaire_objets)+1 or choix < 0:
+            print("cette pokeball n'existe pas")
+        else:
+            return self.inventaire_objets[choix-1]
+
+
     def combat(self, pokemon_joueur, pokemon_sauvage):
         ratio1 = pokemon_joueur.attaque / pokemon_joueur.defense
         ratio2 = pokemon_sauvage.attaque / pokemon_sauvage.defense
@@ -63,6 +78,30 @@ class Game:
             print("Vous avez gagné")
         else:
             print("Vous avez perdu")
+
+    def attraper(self):
+        self.verif = 0
+        self.spawn()
+        print(self.generation[0])
+        while self.verif != 1:
+            pokeball = self.pokeball_choice()
+
+            if pokeball == None:
+                break
+            elif pokeball.nb > 0:
+                nouveau_percent = (self.generation[0].percent_resistance*pokeball.percent)/100
+                rdm = random.randint(0, 100)
+                if rdm >= 0 and rdm <= nouveau_percent:
+                    self.inventaire_pokemon.append(self.generation[0])
+                    pokeball.nb -= 1
+                    self.generation.clear()
+                    print("Vous avez attrapé le pokemon !")
+                    self.verif = 1
+                else:
+                    pokeball.nb -= 1
+                    print("Vous ne l'avez pas attrapé")
+            else:
+                print("Vous n'avez pas cette pokeball")
 
 my_pokemon_list = [
     Pokemon('a', 50, 5, 10, 10),
@@ -79,9 +118,12 @@ my_pokeball_list = [
     Pokeball('masterball', 100),
 ]
 
-a = Game(my_pokemon_list, 100)
+a = Game(my_pokemon_list, my_pokeball_list, 1)
 a.spawn()
 for i in a.generation:
     print(i.name)
 a.calcul_percent()
 # a.combat(my_pokemon_list[1], my_pokemon_list[0])
+my_pokeball_list[2].nb = 500
+a.attraper()
+print(a.inventaire_pokemon[0].name)
