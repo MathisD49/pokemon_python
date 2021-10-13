81a58f8142ff9160d6fcd2cae2fcb8584bde1e21
MathisD49 <mathisdubuc@gmail.com>
2021-10-13 11:39:45 +0200

    ajout de méthodes pour les pokemon, pokeball et pokedollars

--- object/classes/inventaire.py
+++ object/classes/inventaire.py
@@ -4,11 +4,32 @@ class Inventaire:
         self.pokeball = pokeball
         self.pokedollars = pokedollars
 
+    # ---------- POKEMON ----------
     def get_pokemon_list(self):
         return self.pokemon
 
+    def add_pokemon(self, pokemon):
+        self.pokemon.append(pokemon)
+
+    # ---------- POKEBALL ----------
     def get_pokeball_list(self):
         return self.pokeball
+
+    def get_name(self, objet):
+        return objet.name
+
+    def get_nb(self, objet):
+        return objet.nb
+
+    def add_nb(self, objet, nb):
+        objet.nb += nb
+
+    def remove_nb(self, objet, nb):
+        objet.nb -= nb
+
+    # ---------- POKEDOLLARS ----------
+    def get_pokedollars(self):
+        return self.pokedollars
 
     def add_pokedollars(self, nb):
         self.pokedollars += nb
